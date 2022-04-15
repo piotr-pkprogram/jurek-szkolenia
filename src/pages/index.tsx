0858152b7dbce02aa970c47ae31713a5b1bb3c37
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2022-04-15 08:38:00 +0200

    Delete .env.local

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Start Next Template with Typescript And TailwindCss</div>;
+  return <div>Start Next Template with Typescript And TailwindCSS</div>;
 };
 
 export default Home;
